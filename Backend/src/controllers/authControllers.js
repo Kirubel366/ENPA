4152826bd6a045b1db39492addc34e50e5d0cbda
Kirubel Daniel <kirud87@gmail.com>
2025-09-09 16:38:33 +0300

    Login data updated

--- Backend/src/controllers/authControllers.js
+++ Backend/src/controllers/authControllers.js
@@ -108,7 +108,7 @@ export const login = async(req, res) => {
 
         res.status(201).json({
             _id: user._id,
-            name: user.fullName,
+            name: user.name,
             email: user.email,
             phone: user.phone,
             role: user.role
